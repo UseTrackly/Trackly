231abcd5647e833097485c2d9f9b747ee75d9842
Trackly <team@trackly.to>
2026-05-07 11:53:41 -0400

    remove ios folder

--- ios/App/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.base69bfd92e3db7d48eec6c8062.app',
-  appName: 'Trackly - Built For Resellers',
-  webDir: 'www'
-};
-
-export default config;
